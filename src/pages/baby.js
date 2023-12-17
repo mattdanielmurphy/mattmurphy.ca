225f840d60bc5cff1a1da5562c0dff264fb28a98
Matthew Daniel Murphy <matt@mattmurphy.ca>
2023-12-17 00:26:20 -0800

    new strat

--- src/pages/baby.js
+++ src/pages/baby.js
@@ -1,8 +1,8 @@
-"use client"
-
-import { redirect } from 'next/navigation'
+import { useEffect } from 'react'
 export default function Baby() {
-	window.location.replace('https://montessori-beginnings.vercel.app/')
+	useEffect(() => {
+		window.location.replace('https://montessori-beginnings.vercel.app/')
+	})
 	return (
 		<div>
 			<h1>Baby</h1>
