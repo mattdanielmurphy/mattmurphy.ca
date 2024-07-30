bdfaf85365f1724dc275893eb744f517a8c9bce2
Matthew Daniel Murphy <matt@mattmurphy.ca>
2024-07-30 16:18:54 -0700

    add new redirect for jiro dreams of sushi

---
+++ src/pages/api/jiro.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+	res.status(200)
+	res.redirect(
+		301,
+		'https://6ycdj3-my.sharepoint.com/:v:/g/personal/matt_6ycdj3_onmicrosoft_com/Ecrlz6TDnrJCmJbv79nDSjIBt4bZEqsxC_k2lW00vJTybg?e=lfg9KV',
+	)
+}
