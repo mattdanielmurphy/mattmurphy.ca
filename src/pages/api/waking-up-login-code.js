aafc7d37012ea30e77381bf6afb3d8ca3fb527c6
Matthew Daniel Murphy <matt@mattmurphy.ca>
2024-11-15 22:42:56 -0800

    add basic supabase integration

--- src/pages/api/waking-up-login-code.js
+++ src/pages/api/waking-up-login-code.js
@@ -1,12 +1,24 @@
-export default function handler(req, res) {
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = "https://mxjkwzbcgefwdwifpgaz.supabase.co"
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+export default async function handler(req, res) {
 	// Check if the request method is GET
 	if (req.method === "GET") {
-		// Extract query parameters from the request
-		const params = req.query
-
-		const loginCode = decodeURI(params.code).match(/\d*/)
-		// Send the parameters back in the response
-		res.status(200).json({ receivedParams: params, loginCode: loginCode })
+		const { subject } = req.query.params
+		if (subject) {
+			const loginCode = decodeURI(subject).match(/\d*/)
+			if (loginCode) {
+				const { data, error } = await supabase.from("codes").insert([{ loginCode }]).select()
+				if (error) console.log(error)
+				console.log(data)
+				res.status(200).json({ data })
+			}
+		} else {
+			const rows = await supabase.from("codes").select("*")
+			res.status(200).json({ rows })
+		}
 	} else {
 		// Handle any other HTTP method
 		res.setHeader("Allow", ["GET"])
