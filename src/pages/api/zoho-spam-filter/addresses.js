4fb7b46134d326d81d43ec990e575fc2c02722ea
Matthew Daniel Murphy <matt@mattmurphy.ca>
2024-11-23 00:53:43 -0800

    return object with addresses inside it

--- src/pages/api/zoho-spam-filter/addresses.js
+++ src/pages/api/zoho-spam-filter/addresses.js
@@ -16,5 +16,5 @@ export default async function handler(req, res) {
 		res.status(500).json({ error })
 	}
 	const addresses = data.map((row) => row.address)
-	res.status(200).json(addresses)
+	res.status(200).json({ addresses })
 }
