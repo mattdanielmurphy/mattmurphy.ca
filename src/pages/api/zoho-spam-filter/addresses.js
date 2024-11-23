b182cd7045fe8609407ca1c5ad4c0e9868d9f779
Matthew Daniel Murphy <matt@mattmurphy.ca>
2024-11-22 23:50:16 -0800

    add /addresses endpoint

---
+++ src/pages/api/zoho-spam-filter/addresses.js
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = "https://mxjkwzbcgefwdwifpgaz.supabase.co"
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default async function handler(req, res) {
+	if (req.method !== "GET") {
+		res.setHeader("Allow", ["GET"])
+		res.status(405).end(`Method ${req.method} Not Allowed`)
+	}
+
+	const { data, error } = await supabase.from("zoho-spam-filter").select("*")
+	if (error) {
+		console.log(error)
+		res.status(500).json({ error })
+	}
+	const addresses = data.map((row) => row.address)
+	res.status(200).json(addresses)
+}
