432f223e9aa213449789e401f856a6af866ada12
Matthew Daniel Murphy <matt@mattmurphy.ca>
2024-11-22 23:10:42 -0800

    add .end() to api handler so it returns a response

--- src/pages/api/waking-up-login-code.js
+++ src/pages/api/waking-up-login-code.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
 				console.log(data)
 				res.status(200).json({ data })
 			}
-			res.status(200)
+			res.status(200).end()
 		} else {
 			const rows = await supabase.from("waking-up-login-codes").select("*")
 			res.status(200).json({ data: rows.data })
