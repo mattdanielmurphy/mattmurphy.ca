4d0a614f3da6c6037b04b8b103fcab44aba1b60c
Matthew Daniel Murphy <matt@mattmurphy.ca>
2025-12-17 04:10:51 -0700

    change redirect to blockdoku

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	async rewrites() {
-		return [
-			{
-				source: "/blocks",
-				destination: "https://blockdoku.vercel.app/",
-			},
-			{
-				source: "/blocks/:path*",
-				destination: "https://blockdoku.vercel.app/:path*",
-			},
-		]
-	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: "/blocks",
+	// 			destination: "https://blockdoku.vercel.app/",
+	// 		},
+	// 		{
+	// 			source: "/blocks/:path*",
+	// 			destination: "https://blockdoku.vercel.app/:path*",
+	// 		},
+	// 	]
+	// },
 }
 
 module.exports = nextConfig
